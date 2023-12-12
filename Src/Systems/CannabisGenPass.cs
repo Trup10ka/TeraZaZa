df77540de1a8d97159a3a01912a4c55c22c46c37
Trup10ka <lukys3036@gmail.com>
2023-12-12 22:57:31 +0100

    Implement Zaza generation prototype

---
+++ Src/Systems/CannabisGenPass.cs
@@ -0,0 +1,31 @@
+﻿using TeraZaZa.Tile;
+using Terraria;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+using static Terraria.ModLoader.ModContent;
+
+namespace TeraZaZa.Systems;
+
+public class CannabisGenPass : GenPass
+{
+    public CannabisGenPass(string name, double loadWeight) : base(name, loadWeight)
+    {
+    }
+
+    protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+    {
+        progress.Message = "Generating Cannabis";
+
+        var maxToSpawn = (int) (Main.maxTilesX * Main.maxTilesY * 0.05);
+        for (var i = 0; i < maxToSpawn; i++)
+        {
+            var x = WorldGen.genRand.Next(100, Main.maxTilesX - 100);
+            var y = WorldGen.genRand.Next((int) Main.worldSurface - 250, Main.maxTilesY - 900);
+
+            var tile = Framing.GetTileSafely(x, y);
+            if (tile.TileType == TileID.Grass)
+                WorldGen.PlaceTile(x, y - 1, TileType<WhiteWidowCannabis>());
+        }
+    }
+}
---
+++ Src/Systems/SystemCustomization.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace TeraZaZa.Systems;
+
+public class SystemCustomization: ModSystem
+{
+    public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
+    {
+        var index = tasks.FindIndex(pass => pass.Name.Equals("Sunflowers"));
+        if (index != -1)
+        {
+            tasks.Insert(index + 1, new CannabisGenPass("Cannabis", 100f));
+        }
+    }
+}
