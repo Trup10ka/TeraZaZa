ab581c9d2685fd993ec8f3e2d2d5dbbf8469d65a
Trup10ka <lukys3036@gmail.com>
2023-12-13 15:30:25 +0100

    Add other types of Cannabis as Tiles
    - images are fillers

--- Tile/Cannabis/BlueDreamCannabis.cs
+++ Tile/Cannabis/BlueDreamCannabis.cs
@@ -2,20 +2,19 @@
 using TeraZaZa.Items.Zaza;
 using Terraria;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
+
 namespace TeraZaZa.Tile;
 
-public class WhiteWidowCannabis : ModTile
+public class BlueDreamCannabis : ModTile
 {
     public override void SetStaticDefaults()
     {
         Main.tileSolid[Type] = false;
         Main.tileMergeDirt[Type] = false;
         Main.tileBlockLight[Type] = true;
-        
         
         AddMapEntry(new Color(255, 0, 255), CreateMapEntryName());
 
---
+++ Tile/Cannabis/WhiteWidowCannabis.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using TeraZaZa.Items.Zaza;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace TeraZaZa.Tile;
+
+public class WhiteWidowCannabis : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = false;
+        Main.tileMergeDirt[Type] = false;
+        Main.tileBlockLight[Type] = true;
+        
+        AddMapEntry(new Color(255, 0, 255), CreateMapEntryName());
+
+        DustType = DustID.BubbleBurst_Blue;
+        RegisterItemDrop(ItemType<WhiteWidow>());
+    }
+}
