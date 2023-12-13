82ee748eca5a9e1ba337cb7a80a8ba0815925be9
Trup10ka <lukys3036@gmail.com>
2023-12-13 16:56:59 +0100

    Implement stone mortar
    - images are fillers

---
+++ Items/Other/StoneMortar.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TeraZaZa.Items.Other;
+
+public class StoneMortar : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.width = 15;
+        Item.height = 15;
+        Item.maxStack = 9999;
+        Item.value = Item.buyPrice(copper: 50);
+        Item.rare = ItemRarityID.White;
+    }
+    
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient(ItemID.StoneBlock, 10)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
---
+++ Tile/Other/StoneMortar.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace TeraZaZa.Tile.Other;
+
+public class StoneMortar : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = false;
+        Main.tileMergeDirt[Type] = false;
+        Main.tileBlockLight[Type] = false;
+        
+        DustType = DustID.BubbleBurst_Blue;
+        RegisterItemDrop(ItemType<Items.Other.StoneMortar>());
+    }
+}
