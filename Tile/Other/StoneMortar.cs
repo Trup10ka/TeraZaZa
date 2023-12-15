1cb930a7b3eedb7b44544470e54de4bd8fe332dd
Trup10ka <lukys3036@gmail.com>
2023-12-15 07:55:11 +0100

    Fix StoneMortar placement
    - should only be able to be placed on "tables"

--- Tile/Other/StoneMortar.cs
+++ Tile/Other/StoneMortar.cs
@@ -1,4 +1,6 @@
 ﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
@@ -10,13 +12,13 @@ public class StoneMortar : ModTile
 {
     public override void SetStaticDefaults()
     {
-        Main.tileSolid[Type] = false;
+        Main.tileSolid[Type] = true;
         Main.tileSolidTop[Type] = true;
         Main.tileMergeDirt[Type] = false;
         Main.tileBlockLight[Type] = false;
-        Main.tileTable[Type] = true;
         
         TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.Table, 1, 0);
         TileObjectData.addTile(Type);
         
         DustType = DustID.BubbleBurst_Blue;
