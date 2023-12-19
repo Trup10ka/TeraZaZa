161fc30bd862e13662ad65f75bc2d95e45681344
Trup10ka <lukys3036@gmail.com>
2023-12-19 23:14:11 +0100

    Implement custom item drop for Zaza

---
+++ NPC/GlobalNpcCustomization.cs
@@ -0,0 +1,37 @@
+﻿using TeraZaZa.Items.Zaza;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using static Terraria.ModLoader.ModContent;
+using static Terraria.ID.NPCID;
+
+using TerrariaNPC = Terraria.NPC;
+
+namespace TeraZaZa.Npc;
+
+public class GlobalNpcCustomization : GlobalNPC
+{
+    public override void ModifyNPCLoot(TerrariaNPC npc, NPCLoot npcLoot)
+    {
+        if (!IsValidNPC(npc)) return;
+        
+        npcLoot.Add(
+            ItemDropRule
+                .OneFromOptions(10, ItemType<WhiteWidow>(), ItemType<MauiWowie>())
+            );
+        npcLoot.Add(
+            ItemDropRule
+                .OneFromOptions(10, ItemType<WhiteWidow>(), ItemType<MauiWowie>())
+        );
+    }
+
+    private static bool IsValidNPC(TerrariaNPC npc)
+    {
+        return npc.type is 
+            Zombie or 
+            BigZombie or 
+            SnowFlinx or 
+            FemaleZombie;
+    }
+}
