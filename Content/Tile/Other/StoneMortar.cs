566f645ea8a05ab0453290756429d4fdd2cb9460
Trup10ka <lukys3036@gmail.com>
2023-12-22 08:29:16 +0100

    Update StoneMortar tile

--- Content/Tile/Other/StoneMortar.cs
+++ Content/Tile/Other/StoneMortar.cs
@@ -12,13 +12,14 @@ public class StoneMortar : ModTile
 {
     public override void SetStaticDefaults()
     {
-        Main.tileSolid[Type] = true;
-        Main.tileSolidTop[Type] = true;
+        Main.tileSolid[Type] = false;
+        Main.tileSolidTop[Type] = false;
         Main.tileMergeDirt[Type] = false;
         Main.tileBlockLight[Type] = false;
+        Main.tileFrameImportant[Type] = true;
         
         TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
-        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.Table, 1, 0);
+        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.Table, TileObjectData.newTile.Width, 0);
         TileObjectData.addTile(Type);
         
         DustType = DustID.BubbleBurst_Blue;
