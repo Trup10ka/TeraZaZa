024ae954e43d080f37a1f807feac17d731a16a84
Trup10ka <lukys3036@gmail.com>
2023-12-22 16:55:27 +0100

    Update Stone Mortar size and animation

--- Content/Items/Other/StoneMortar.cs
+++ Content/Items/Other/StoneMortar.cs
@@ -9,12 +9,12 @@ public class StoneMortar : ModItem
 {
     public override void SetDefaults()
     {
-        Item.useTime = 10;
-        Item.useAnimation = 10;
+        Item.useTime = 15;
+        Item.useAnimation = 15;
         Item.autoReuse = true;
         Item.useStyle = ItemUseStyleID.Swing;
-        Item.width = 8;
-        Item.height = 8;
+        Item.width = 15;
+        Item.height = 15;
         Item.maxStack = 9999;
         Item.value = Item.buyPrice(copper: 50);
         Item.rare = ItemRarityID.White;
